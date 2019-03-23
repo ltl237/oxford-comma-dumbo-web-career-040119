53a618c1aade86a0592907487286aa86306773b4
Lorenzo Llamas <ltl924@gmail.com>
2019-03-23 21:33:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
-  if array.length > 2
-    array.join(', and ')
-  else
+  if array.length < 3
     array.join(' and ')
+  else
+    array[0:(array.length-1)].join(', ')
+    
   end
 end
