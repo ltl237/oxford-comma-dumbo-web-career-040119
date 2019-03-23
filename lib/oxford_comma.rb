8229d05fcf74146928cbd22dcd6c850c2bc08d82
Lorenzo Llamas <ltl924@gmail.com>
2019-03-23 21:37:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   last_ele = array[array.length-1]
   if array.length < 3
-    puts array.join(' and ')
+    array.join(' and ')
   else
     array.pop
     #array.join(', ')
